9c67958a8480c7984b09d988e2a07c1e28bae6d8
lauraheerrera <159728136+lauraheerrera@users.noreply.github.com>
2025-05-03 20:16:31 +0100

    feat: changed member input in second step

--- src/app/pages/event-creation-pages/event-creation-second/event-creation-second-form/event-creation-second-form.component.ts
+++ src/app/pages/event-creation-pages/event-creation-second/event-creation-second-form/event-creation-second-form.component.ts
@@ -2,11 +2,12 @@ import {Component, OnInit} from '@angular/core';
 import {UsersListComponent} from '../../../../components/users-list/users-list.component';
 import {User} from '../../../../../architecture/model/User';
 import {FormService} from '../../../../services/form.service';
+import {UsersSearchInputComponent} from '../../../../components/users-search-input/users-search-input.component';
 
 @Component({
   selector: 'app-event-creation-second-form',
   standalone: true,
-  imports: [UsersListComponent],
+  imports: [UsersListComponent, UsersSearchInputComponent],
   templateUrl: './event-creation-second-form.component.html',
   styleUrl: './event-creation-second-form.component.css'
 })
