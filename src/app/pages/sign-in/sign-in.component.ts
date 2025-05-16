a88831bf844a4eaa5360aa5479952dbf9f158fc6
Laura <laurahn07@gmail.com>
2025-05-16 23:08:05 +0100

    fix: add validation form in sign-in page

--- src/app/pages/sign-in/sign-in.component.ts
+++ src/app/pages/sign-in/sign-in.component.ts
@@ -1,28 +1,41 @@
-import {Component} from '@angular/core';
-import {Router} from '@angular/router';
-import {ServiceFactory} from "../../services/service-factory.service";
-import {AuthenticationService} from "../../../architecture/io/services/AuthenticationService";
-import {FormsModule} from "@angular/forms";
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ServiceFactory } from '../../services/service-factory.service';
+import { AuthenticationService } from '../../../architecture/io/services/AuthenticationService';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
-    selector: 'app-sign-in',
-    imports: [
-        FormsModule
-    ],
-    templateUrl: './sign-in.component.html',
-    styleUrl: './sign-in.component.css'
+  selector: 'app-sign-in',
+  imports: [FormsModule],
+  templateUrl: './sign-in.component.html',
+  styleUrl: './sign-in.component.css',
 })
 export class SignInComponent {
-    protected email: string = "";
-    protected password: string = "";
+  protected email: string = '';
+  protected password: string = '';
+  protected invalidCredentials: boolean = false;
 
-    constructor(
-        private serviceFactory: ServiceFactory,
-        private router: Router
-    ) {
+  constructor(
+    private serviceFactory: ServiceFactory,
+    private router: Router
+  ) {}
+
+  protected signIn(form: NgForm) {
+    this.invalidCredentials = false;
+
+    if (form.invalid) {
+      return;
     }
 
-    protected signIn() {
-        (this.serviceFactory.get('auth') as AuthenticationService).signIn(this.email, this.password).subscribe(() => this.router.navigate(['/homePage']).then());
-    }
+    (this.serviceFactory.get('auth') as AuthenticationService)
+      .signIn(this.email, this.password)
+      .subscribe({
+        next: () => {
+          this.router.navigate(['/homePage']).then();
+        },
+        error: () => {
+          this.invalidCredentials = true;
+        },
+      });
+  }
 }
