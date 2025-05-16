8c9602c82cdcaf09bea99e9085c3c4ab7efa3534
Laura <laurahn07@gmail.com>
2025-05-16 22:50:56 +0100

    feat: home-page implemented with ionic

--- android/app/src/main/java/com/join_up/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.join_up.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,13 +10,15 @@ import {FirebaseMessageService} from "./io/services/FirebaseMessageService";
 import {FooterComponent} from './components/footer/footer.component';
 import {FirebaseEventService} from "./io/services/FirebaseEventService";
 import {FirebaseGroupService} from "./io/services/FirebaseGroupService";
+import {IonContent} from '@ionic/angular/standalone';
 
 @Component({
     selector: 'app-root',
   imports: [
     RouterOutlet,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    IonContent
   ],
     templateUrl: './app.component.html',
     standalone: true,
